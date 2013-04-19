039aded278cf361d6b383ef75ecf48b49faa7b59
jamesh <jameshbabu@gmail.com>
2013-04-19 11:33:47 +0530

    initial commit

---
+++ calmation/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calmation::Application.config.secret_token = '0c24538bc9ed6494257d558a6d2802606ea325ce73d285218153d729f1014e470606eec80967e2556a5248764cccea58c7bd5c78eb711945f1fd0135a79d7d8d'
---
+++ calmation/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Calmation::Application.config.session_store :cookie_store, key: '_calmation_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Calmation::Application.config.session_store :active_record_store
